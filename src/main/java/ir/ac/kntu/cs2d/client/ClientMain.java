21a6b5ac5640a652cbb9fa8f0c56ae53df4189d9
Seyyed Shayan Daneshvar <daneshvarshayan@gmail.com>
2020-06-28 18:51:47 +0430

    Initial commit

---
+++ src/main/java/ir/ac/kntu/cs2d/client/ClientMain.java
@@ -0,0 +1,10 @@
+package ir.ac.kntu.cs2d.client;
+
+public class ClientMain {
+    public static void main(String[] args) {
+        // TODO
+    }
+
+    private ClientMain() {
+    }
+}
---
+++ src/main/java/ir/ac/kntu/cs2d/server/ServerMain.java
@@ -0,0 +1,10 @@
+package ir.ac.kntu.cs2d.server;
+
+public class ServerMain {
+    public static void main(String[] args) {
+        // TODO
+    }
+
+    private ServerMain() {
+    }
+}
